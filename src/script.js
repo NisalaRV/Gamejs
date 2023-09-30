bb938a86e06b6ce3e1ba9ff1923286cb1d27265d
NisalaRV <nisalaravihara129@gmail.com>
2023-09-30 23:37:09 +0530

    Script file added

---
+++ src/script.js
@@ -0,0 +1,47 @@
+const board = [
+    null, 0, null, 1, null, 2, null, 3,
+    4, null, 5, null, 6, null, 7, null,
+    null, 8, null, 9, null, 10, null, 11,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    12, null, 13, null, 14, null, 15, null,
+    null, 16, null, 17, null, 18, null, 19,
+    20, null, 21, null, 22, null, 23, null
+]
+
+// --
+let redsPieces = document.querySelectorAll("p");
+let blacksPieces = document.querySelectorAll("span")
+let playerPieces;
+
+let turn = true;
+
+function givePiecesEventListeners() {
+    if (turn) {
+        for (let i = 0; i < redsPieces.length; i++) {
+            redsPieces[i].addEventListener("click", getPlayerPieces);
+        }
+    } else {
+        for (let i = 0; i < blacksPieces.length; i++) {
+            blacksPieces[i].addEventListener("click", getPlayerPieces);
+        }
+    }
+}
+
+givePiecesEventListeners();
+
+function getPlayerPieces() {
+    if (turn) {
+        playerPieces = redsPieces;
+    } else {
+        playerPieces = blacksPieces;
+    }
+    resetBorders();
+}
+
+function resetBorders() {
+    for (let i = 0; i < playerPieces.length; i++) {
+        playerPieces[i].style.border = "1px solid white";
+    }
+}
+
