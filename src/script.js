0c2f4e32a6d7fc1cb03f90e289c9fc66b06613b0
NisalaRV <nisalaravihara129@gmail.com>
2023-10-02 21:18:34 +0530

    checkPieceConditions

--- src/script.js
+++ src/script.js
@@ -170,8 +170,24 @@ function checkAvailableJumpSpaces() {
             selectedPiece.minusEighteenthSpace = true;
         }
     }
-
+    checkPieceConditions();
 }
+function checkPieceConditions() {
+    if (selectedPiece.isKing) {
 
+    } else {
+        if (turn) {
+            selectedPiece.minusSeventhSpace = false;
+            selectedPiece.minusNinthSpace = false;
+            selectedPiece.minusFourteenthSpace = false;
+            selectedPiece.minusEighteenthSpace = false;
+        } else {
+            selectedPiece.seventhSpace = false;
+            selectedPiece.ninthSpace = false;
+            selectedPiece.fourteenthSpace = false;
+            selectedPiece.eighteenthSpace = false;
+        }
 
+    }
+}
 
